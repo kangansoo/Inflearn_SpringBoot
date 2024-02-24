6480fd7f7e8dbfb64dc7e13786749678940d630e
kangansoo <ansoo971@gamail.com>
2024-02-25 00:27:30 +0900

    day5

---
+++ src/main/java/com/group/libraryapp/repository/user/UserRepository.java
@@ -0,0 +1,51 @@
+package com.group.libraryapp.repository.user;
+
+import com.group.libraryapp.dto.user.response.UserResponse;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class UserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean isUserNotExist(long id){
+        // 분리를 했기 때문에 굳이 객체(request)를 받아오지 않고 특정 컬럼만 받아옴
+        String readSql = "SELECT * FROM user WHERE id = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum) -> 0, id).isEmpty();
+    }
+
+    public void updateUserName(String name, long id){
+        String sql = "update user set name = ? where id = ?";
+        jdbcTemplate.update(sql, name, id);
+    }
+
+    public boolean isUserNotExist(String name){
+        String readSql = "select * from user where name = ?";
+        return jdbcTemplate.query(readSql, (rs, rowNum)-> 0, name).isEmpty();
+    }
+
+    public void deleteUser(String name){
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
+
+    public void saveUser(String name, Integer age){
+        String sql = "INSERT INTO user (name, age) VALUES (?, ?)";
+        jdbcTemplate.update(sql, name, age);
+    }
+
+    public List<UserResponse> getUsers(){
+        String sql = "SELECT * FROM user";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> {
+            long id = rs.getLong("id");
+            String name = rs.getString("name");
+            int age = rs.getInt("age");
+            return new UserResponse(id, name, age);
+        });
+    }
+}
