b6f8d30dbaa7af1bd73fed1f31b6721e8ef9ae4b
kangansoo <ansoo971@gmail.com>
2024-03-01 23:56:11 +0900

    9일차 수업

---
+++ src/main/java/com/group/libraryapp/domain/book/BookRepository.java
@@ -0,0 +1,5 @@
+package com.group.libraryapp.domain.book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -1,20 +1,22 @@
 package com.group.libraryapp.service.book;
 
-import com.group.libraryapp.repository.book.BookMemoryRepository;
-import com.group.libraryapp.repository.book.BookMySqlRepository;
-import com.group.libraryapp.repository.book.BookRepository;
+import com.group.libraryapp.domain.book.Book;
+import com.group.libraryapp.domain.book.BookRepository;
+import com.group.libraryapp.dto.book.request.BookCreateRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class BookService {
 
-    private final BookRepository bookRepository ; //<-new BookMemoryRepository();
+    private final BookRepository bookRepository ;
 
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    public void saveBook(){
-        bookRepository.saveBook();
+    @Transactional
+    public void saveBook(BookCreateRequest request){
+        bookRepository.save(new Book(request.getName()));
     }
 }
